c60d56b5ea931a30d8f8acdae2a5442e9a8da559
Dmitry Khokhlov <dmitry.hohlov@mail.ru>
2014-02-16 23:44:43 +0400

    FastSort: добавить начальную версию проекта

---
+++ FastSort/CharNode.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FastSort
+{
+    class CharNode
+    {
+        private SortedDictionary<char, CharNode> _children = new SortedDictionary<char, CharNode>(new Comparer<char>());
+
+        private bool _isFullWord = false;
+
+        public CharNode GetChild(char childChar)
+        {
+            CharNode child;
+            if (!_children.TryGetValue(childChar, out child))
+            {
+                child = new CharNode();
+                _children.Add(childChar, child);
+            }
+            return child;
+        }
+
+        public void MarkAsFullWord()
+        {
+            _isFullWord = true;
+        }
+
+        public void FillSortedWordsList(string wordPrefix, List<string> wordList)
+        {
+            if (_isFullWord)
+            {
+                wordList.Add(wordPrefix);
+            }
+
+            foreach (KeyValuePair<char, CharNode> childKeyValuePair in _children)
+            {
+                childKeyValuePair.Value.FillSortedWordsList(wordPrefix + childKeyValuePair.Key, wordList);
+            }
+        }
+    }
+}
---
+++ FastSort/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FastSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine('о' > 'м');
+            return;
+
+            CharNode rootNode = new CharNode();
+            CharNode currentNode = rootNode;
+            int receivedCharCode;
+            bool isWordStarted = false;
+
+            while ( (receivedCharCode = Console.Read()) != -1)
+            {
+                char receivedChar = Convert.ToChar(receivedCharCode);
+                switch (receivedChar)
+                {
+                    case ' ':
+                    case '\n':
+                    case '\r':
+                    case '\t':
+                    case '\v':
+                        if (isWordStarted)
+                        {
+                            currentNode.MarkAsFullWord();
+                            currentNode = rootNode;
+                            isWordStarted = false;
+                        }
+                        break;
+
+                    default:
+                        currentNode = currentNode.GetChild(receivedChar);
+                        isWordStarted = true;
+                        break;
+                }
+            }
+            if (isWordStarted)
+            {
+                currentNode.MarkAsFullWord();
+            }
+            
+            List<string> sortedWordList = new List<string>();
+            rootNode.FillSortedWordsList("", sortedWordList);
+
+            foreach (string word in sortedWordList)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
